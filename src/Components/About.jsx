5ddda543b999dcbfc04c78b5b2d0fcd27320a11d
Vishvaa K <kvishvaa6@gmail.com>
2024-09-28 06:21:10 +0000

    Update styles.css and index.html, and modify package.json, App.jsx, Home.jsx, About.jsx, and Portfolio.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,22 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a engineering student studying at Velammal Engineering College. I enjoy creating user-friendly full stack web applications which are very useful and intuitive.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "JavaScript",
+  "React",
+  "MongoDB",
+  "Flask",
+  "SQL",
+  "UI/UX Design",
+  "HTML/CSS",
+  "Bootstrap",
 ];
 
 /**
@@ -45,7 +48,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving real time problems in a creative and intuitive way with my skills. I am a quick learner and always open to new technologies and ideas.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "EcoBallot",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "EcoBallot, a platform designed to revolutionize the way students participate in school elections. This app was created in Python using the Flask Framework.",
+    url: "https://github.com/vishvaa-vsk/EcoBallot",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Comphrensive Language Skills Assessment System",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "An online application that lets institutes upload listening and comprehension tests through the Excel file and audio and automates language assessment.",
+    url: "https://github.com/vishvaa-vsk/Quiz-App",
   },
 ];
 
